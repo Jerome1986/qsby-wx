17049022c6bb08fbe09420f2659b68311e0149ac
jeromeZj0094 <jeromeZj0094@noreply.gitcode.com>
2026-02-03 10:57:35 +0800

    项目初始化

---
+++ src/pages/activity/activity.vue
@@ -0,0 +1,7 @@
+<template>
+  <view> 活动 </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/goPlay/goPlay.vue
@@ -0,0 +1,7 @@
+<template>
+  <view> 趣哪游 </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
